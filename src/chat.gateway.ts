07e7e6069b2352a8eeb8172e1a4eb4b19d174430
rohitbajaj7877 <rohitbajaj7877@gmail.com>
2023-02-22 19:22:45 +0530

    changes in chat api

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -2,21 +2,19 @@ import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
 import { MessageBody, WebSocketServer } from "@nestjs/websockets/decorators";
 
 @WebSocketGateway() // namespace is to separate this chat gateway from other gateways
-export class ChatGateway{
+export class ChatGateway {
 
     @WebSocketServer()
     socket;
 
     @SubscribeMessage('chat') // name of the message we are listening to from client side
-    handleMessage(@MessageBody() message:Object): void
-    {    
-        
+    handleMessage(@MessageBody() message: Object): void {
+
+        console.log("sending message to ", message['receiverID']);
         var url = `chat/${message['receiverID']}`;
-       
+
         this.socket.emit(url, JSON.stringify(message));
     }
 
 
-    
-    
 }
