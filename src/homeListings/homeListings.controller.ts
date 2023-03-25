13d7e82e8335cb498f4eb1763c3ce591cf530829
HarshaK47 <harsha49code@gmail.com>
2023-03-25 20:40:17 +0530

    Home Listings fetch with active status api

---
+++ src/homeListings/homeListings.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Param } from "@nestjs/common";
+import { ObjectId } from "mongoose";
+import { ObjectIdPipe } from "src/pipes/object-id.pipe";
+
+import { HomeListingsService } from "./homeListings.services";
+
+@Controller('homeListings')
+export class HomeListingsController {
+    constructor(private readonly listingsService: HomeListingsService) { }
+
+    @Get('homeproducts')
+    async getProducts() {
+        return await this.listingsService.getProducts();
+    }
+
+    
+
+    }
---
+++ src/homeListings/homeListings.services.ts
@@ -0,0 +1,28 @@
+import { Model, ObjectId } from "mongoose";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Listing, listingDocument } from "../schemas/listing.schema";
+import { User, userDocument } from "../schemas/user.schema";
+
+
+@Injectable()
+export class HomeListingsService {
+    constructor(@InjectModel(Listing.name) private listingModel: Model<listingDocument>,
+        @InjectModel(User.name) private userModel: Model<userDocument>) { }
+
+    async getProducts(): Promise<any>{
+        
+        try {
+            const listings = await this.listingModel.find({status: "Active"});
+            return listings;
+        }
+        catch (err) {
+            console.log(err);
+            return err;
+        }
+
+    }
+
+    
+}
+
