b818d8a9d76a8870055c2c516608192f2e37f7b4
rohitbajaj7877 <rohitbajaj7877@gmail.com>
2023-03-26 19:35:17 +0530

    modified getProducts api

--- src/homeListings/homeListings.services.ts
+++ src/homeListings/homeListings.services.ts
@@ -10,11 +10,18 @@ export class HomeListingsService {
     constructor(@InjectModel(Listing.name) private listingModel: Model<listingDocument>,
         @InjectModel(User.name) private userModel: Model<userDocument>) { }
 
-    async getProducts(uid: ObjectId): Promise<any>{
-        
+    async getProducts(uid: ObjectId): Promise<any> {
+
         try {
-            const listings = await this.listingModel.find({status: "Active", owner: {$ne: uid}});
-            return listings;
+            const listings = await this.listingModel.find({ status: "Active", owner: { $ne: uid } });
+            const user = await this.userModel.findById(uid, 'itemsLiked itemsRequested');
+            // return [listings, user.itemsLiked, user.itemsRequested];
+            return {
+                listings: listings,
+                itemsLiked: user.itemsLiked,
+                itemsRequested: user.itemsRequested
+            };
+
         }
         catch (err) {
             console.log(err);
@@ -24,39 +31,39 @@ export class HomeListingsService {
     }
 
 
-    async toggleLikeStatus(listingID: ObjectId, uid: ObjectId): Promise<any>{
-        
-        try{
+    async toggleLikeStatus(listingID: ObjectId, uid: ObjectId): Promise<any> {
+
+        try {
             var found = "false";
-            
+
             const listing = await this.listingModel.findById(listingID);
             const user = await this.userModel.findById(uid);
-            user.itemsLiked.map((item)=>{
-                if(item.toString()==listing._id.toString()){
+            user.itemsLiked.map((item) => {
+                if (item.toString() == listing._id.toString()) {
                     found = "true";
                 }
             });
 
-            if(found==="false"){
+            if (found === "false") {
                 user.itemsLiked.push(listing._id);
                 listing.likes = +listing.likes + Number(1);
-            }else{
-                user.itemsLiked = user.itemsLiked.filter((item)=>{
-                    return item.toString()!==listing._id.toString();
+            } else {
+                user.itemsLiked = user.itemsLiked.filter((item) => {
+                    return item.toString() !== listing._id.toString();
                 });
                 listing.likes = +listing.likes - Number(1);
             }
             await user.save();
             await listing.save();
-            return {listing,user};
+            return { listing, user };
 
         }
-        catch(err){
+        catch (err) {
             console.log(err);
             return err;
         }
     }
 
-    
+
 }
 
